e0c58855b24f559aea7289c3b3aee6b132aa6c5a
nishizawa <nishizawa@timedia.co.jp>
2019-03-29 09:50:55 +0900

    プロジェクト ファイルを追加します。

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        [Display(Name ="ID")]
+        public int ID { get; set; }
+
+        [Display(Name ="タイトル")]
+        public string Title { get; set; }
+
+        [Display(Name ="公開日")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name ="ジャンル")]
+        public string Genre { get; set; }
+        [Display(Name ="価格")]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
