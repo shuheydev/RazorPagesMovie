458d71cce6a7f59109da8dcbde6c73aa274b6669
nishizawa <nishizawa@timedia.co.jp>
2019-03-29 16:16:03 +0900

    https://docs.microsoft.com/ja-jp/aspnet/core/tutorials/razor-pages/validation?view=aspnetcore-3.0 完了。

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -9,20 +9,33 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-        [Display(Name ="ID")]
         public int ID { get; set; }
 
+        [StringLength(60,MinimumLength =3)]
+        [Required]
         [Display(Name ="タイトル")]
         public string Title { get; set; }
 
         [Display(Name ="公開日")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Display(Name = "価格")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         [Display(Name ="ジャンル")]
         public string Genre { get; set; }
-        [Display(Name ="価格")]
-        [Column(TypeName ="decimal(18,2)")]
-        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        [Display(Name ="レーティング")]
         public string Rating { get; set; }
     }
 }
